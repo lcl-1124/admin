b38d621bd85d612dbbf154f54ab6521d3c980ed7
lcl <lichenle1124@qq.com>
2020-01-14 21:46:52 +0800

    10 commit

--- src/pages/admin/charts/pie.jsx
+++ src/pages/admin/charts/pie.jsx
@@ -1,12 +1,177 @@
 /*
 饼图路由
 */
-import React,{Component} from 'react'
+import React from 'react'
+import {Card,Button} from 'antd'
+import ReactEcharts from 'echarts-for-react'
 
-export default class Pie extends Component {
+export default class Pie extends React.Component {
+  state = {
+    scales: [
+      {value: 335, name: '衬衫'},
+      {value: 310, name: '羊毛衫'},
+      {value: 274, name: '雪纺衫'},
+      {value: 235, name: '裤子'},
+      {value: 400, name: '高跟鞋'},
+      {value: 400, name: '袜子'}
+    ], // 单货销售总量列表
+    stores: [
+      {value: 335, name: '衬衫'},
+      {value: 310, name: '羊毛衫'},
+      {value: 274, name: '雪纺衫'},
+      {value: 235, name: '裤子'},
+      {value: 400, name: '高跟鞋'},
+      {value: 400, name: '袜子'}
+    ], // 单货库存总量列表
+  }
+  // 更新数据
+  updateData = () => {
+    let {scales,stores} = this.state
+
+    
+    scales = scales.map(scale => {
+      scale.value = Math.round(Math.random()*520+80)
+      return scale
+    })
+
+    stores = stores.reduce((pre,store) => {
+      console.log('math前',store.value)
+      store.value = Math.round(Math.random()*520+80)
+      console.log('math后',store.value,store)
+      pre.push(store)
+      return pre
+    },[])
+
+    this.setState((state) => {
+      return {
+        scales: state.scales,
+        stores: state.stores
+      }
+    })
+  }
+  // 获取option1
+  getScalesOption = (scales) => {
+    return {
+      backgroundColor: '#2c343c',
+  
+      title: {
+          text: '货品总销售比例',
+          left: 'center',
+          top: 20,
+          textStyle: {
+              color: '#ccc'
+          }
+      },
+  
+      tooltip: {
+          trigger: 'item',
+          formatter: '{a} <br/>{b} : {c} ({d}%)'
+      },
+  
+      visualMap: {
+          show: false,
+          min: 80,
+          max: 600,
+          inRange: {
+              colorLightness: [0, 1]
+          }
+      },
+      series: [
+          {
+              name: '货品总销售比例',
+              type: 'pie',
+              radius: '55%',
+              center: ['50%', '50%'],
+              data: scales.sort(function (a, b) { return a.value - b.value; }),
+              roseType: 'radius',
+              label: {
+                  color: 'rgba(255, 255, 255, 0.3)'
+              },
+              labelLine: {
+                  lineStyle: {
+                      color: 'rgba(255, 255, 255, 0.3)'
+                  },
+                  smooth: 0.2,
+                  length: 10,
+                  length2: 20
+              },
+              itemStyle: {
+                  color: '#c23531',
+                  shadowBlur: 200,
+                  shadowColor: 'rgba(0, 0, 0, 0.5)'
+              },
+  
+              animationType: 'scale',
+              animationEasing: 'elasticOut',
+              animationDelay: function (idx) {
+                  return Math.random() * 200;
+              }
+          }
+      ]
+    }
+  }
+  // 获取option2
+  getStoresOption2 = (stores) => {
+    return {
+      title: {
+          text: '货品总库存比例',
+          subtext: '纯属虚构',
+          left: 'center'
+      },
+      tooltip: {
+          trigger: 'item',
+          formatter: '{a} <br/>{b} : {c} ({d}%)'
+      },
+      legend: {
+          orient: 'vertical',
+          left: 'left',
+          data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+      },
+      visualMap: {
+        show: false,
+        min: 80,
+        max: 600,
+        inRange: {
+            colorLightness: [0, 1]
+        }
+      },
+      series: [
+          {
+              name: '货品总库存比例',
+              type: 'pie',
+              radius: '55%',
+              center: ['50%', '60%'],
+              data: stores,
+              emphasis: {
+                  itemStyle: {
+                      shadowBlur: 10,
+                      shadowOffsetX: 0,
+                      shadowColor: 'rgba(0, 0, 0, 0.5)'
+                  }
+              },
+              animationType: 'scale',
+              animationEasing: 'elasticOut',
+              animationDelay: function (idx) {
+                  return Math.random() * 200;
+              }
+          }
+      ]
+    }
+  }
   render () {
+    const {scales,stores} = this.state
     return (
-      <div>Pie</div>
+      <div>
+        <Card>
+          <Button type='primary' onClick={this.updateData}>更新</Button>
+        </Card>
+        <Card>
+          <ReactEcharts option={this.getScalesOption(scales)} style={{height: 385}}/>
+        </Card>
+        <Card>
+          <ReactEcharts option={this.getStoresOption2(stores)} style={{height: 385}}/>
+        </Card>
+      </div>
     )
   }
 }
--- src/pages/admin/home/home.jsx
+++ src/pages/admin/home/home.jsx
@@ -2,13 +2,40 @@
 首页路由
 */
 import React,{Component} from 'react'
-
-import './home.less'
+import { Statistic, Card, Icon } from 'antd'
 
 export default class Home extends Component {
   render () {
+    const title = (
+      <Card size="small" title="商品总量" extra={<Icon type="smile" theme="twoTone" />} style={{ width: 260 ,height: 200,boxSizing: "border-box",padding: 20}}>
+        <Statistic 
+          value={112893256}  
+          valueStyle={{ color: '#000', fontSize: 28}}
+          suffix='个'
+        />
+        <Statistic
+          value={10 + "%"}
+          valueStyle={{ color: '#000' , fontSize: 15,opacity:.6}}
+          prefix='日同比'
+          suffix={<Icon type="fall" />}
+        />
+        <Statistic
+          value={ 15 + "%"}
+          valueStyle={{ color: '#000', fontSize: 15,margin: '10px 0',opacity:.6}}
+          prefix='周同比'
+          suffix={<Icon type="rise" />}
+        />
+      </Card>
+    )
+    const extra = (
+      <div></div>
+    )
     return (
-      <div className='home'>欢迎来到梅洛天庭</div>
+      <div className='home'>
+        <Card title={title} extra={extra}>
+          
+        </Card>
+      </div>
     )
   }
 }
