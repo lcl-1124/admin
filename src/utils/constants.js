640ec86d79e01ef330bca4a000c50db98215c216
lcl <lichenle1124@qq.com>
2020-01-15 19:54:15 +0800

    finish commit

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 /*
 常量定义工具
 */
-export const PAGE_NUM = 2
+export const PAGE_NUM = 4
 export const DEFAULT_PAGE_SIZE = ['4','6','8','10']
 export const BASE_IMG_URL = 'http://localhost:5000/upload/'
